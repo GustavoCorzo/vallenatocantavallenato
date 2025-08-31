07b7e2ec6d0d39b98673b7d6e20ca9b7498542f2
Tavo Corzo <guscorzo09@gmail.com>
2025-08-31 09:06:58 -0500

    Previniendo olvidos seniles

--- mivalle_main/settings.py
+++ mivalle_main/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = config('DJANGO_ALLOWED_HOSTS', default='127.0.0.1,localhost').split(',')
+ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS').split(',')
 
 # Application definition
 
